877ce87e60aa685324d7d1b08f7660a8ed60e28b
jencekade <gaephuoc@gmail.com>
2017-12-24 09:15:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('div#nested div div div div.target');
+}
+
+function deepestChild() {
+  let node = document.getElementById('grand-node');
+  let nextNode = node.children[0];
+  while(nextNode) {
+    node = nextNode;
+    nextNode = node.children[0];
+  }
+  return node;
+}
+
+function increaseRankBy(n) {
+  const increaseList = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < increaseList.length; i++) {
+    increaseList[i].innerHTML = parseInt(increaseList[i].innerHTML) + n;
+  }
+}
